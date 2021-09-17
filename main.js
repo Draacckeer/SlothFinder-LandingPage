3df22e045e5a5ac4a69e9f92b523d0eb43275077
Draacckeer <90934862+Draacckeer@users.noreply.github.com>
2021-09-17 18:05:37 -0500

    feat: responsive navbar made
    the navbar was made following the landing page mockup, what remains is to add the links to the sections that are not ready yet

---
+++ main.js
@@ -0,0 +1,10 @@
+document.getElementById("toggle-button").addEventListener("click", () => {
+    document.getElementById("menu").classList.toggle("active");
+});
+
+let anchors = document.getElementsByTagName("a");
+for(let i = 0; i < anchors.length; i++) {
+    anchors[i].addEventListener("click", () => {
+        document.getElementById("menu").classList.toggle("active");
+    })
+}
